5a36f4c8bb4f984ae741e0bdc3ecfa6f536798b3
Teddy-Hung <tedhungkk@gmail.com>
2020-12-07 16:16:39 -0800

    finished higher order section

--- higher-order.js
+++ higher-order.js
@@ -21,7 +21,11 @@ const mixedNumbers = [6,3,1,7,5,2,6,8,9,4,2,7,9,3,1,8,4,3];
 */
 
 //Code Here
-let evenNumbers // = mixedNumbers.filter(/* Provide Your Callback Here */)
+let evenNumbers = mixedNumbers.filter(e=> {
+  if(e % 2 === 0){
+    return e
+  }
+})
 
 
 
@@ -44,9 +48,12 @@ const prices = [15.00, 23.00, 78.00, 34.00, 12.00, 86.00, 12.00, 79.00, 32.00];
 */
 
 //Code Here
-let postTaxPrices // = prices.map(/* Provide Your Callback Here );
+let postTaxPrices = prices.map(e=> e *= 1.07)
 
-
+// let postTaxPrices = prices.map(function(element){
+//   element = element*1.07
+//   return element
+// })
 
 ////////// PROBLEM 3 //////////
 
@@ -63,7 +70,8 @@ const populations = [8175133, 3792621, 2695598, 2100263];
 */
 
 //Code Here
-let totalPopulation //  = populations.reduce(/* Provide Your Callback Here */)
+//acc = accumulator, current is current element
+let totalPopulation  = populations.reduce((acc, curr) => acc += curr)
 
 
 
@@ -89,7 +97,7 @@ const monstersInYourPocket = [{"monster":"Bulbabunny","CP":156},{"monster":"Bulb
 */
 
 //Code Here
-let myStrongest // = monstersInYourPocket.filter(/* Provide Your Callback Here */)
+let myStrongest = monstersInYourPocket.filter(e=> e.CP > 200)
 
 
 
@@ -103,10 +111,11 @@ const orders = [{"price":15,"tax":0.09},{"price":42,"tax":0.07},{"price":56,"tax
 // Do not edit code above.
 
 /*
-  Use a higher order method to get all the order totals after adding in the sales tax (given to you as a tax rate, hint: you'll need to do some multiplication). Your answer should be an array of numbers, one total for each order.
+  Use a higher order method to get all the order totals after adding in the sales tax (given to you as a tax rate, 
+    hint: you'll need to do some multiplication). Your answer should be an array of numbers, one total for each order.
 */
 
-let orderTotals // Code here
+let orderTotals = orders.map(e => e.price * (1.0+e.tax))
 
 
 
@@ -126,6 +135,11 @@ const purchases = [{"owner":"Barry","price":103},{"owner":"Bob","price":75},
   Use a high order method to create to get the sum of bobsTotal.
 */
 
-let bobsTotal //Code Here
+let bobsTotal = purchases.reduce((acc, curr) =>{
+  if(curr.owner === 'Bob'){
+    acc += curr.price
+  }
+  return acc//need return statement because don't have implied return(one line arrow function statement)
+},0)//accumulator doesn't take in the first element
 
 
